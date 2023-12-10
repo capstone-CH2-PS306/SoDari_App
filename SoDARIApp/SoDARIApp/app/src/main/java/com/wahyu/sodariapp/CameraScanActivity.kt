2bf42d4211a0a4c8b0265295cac80f190c1f38b6
wahyuals <101552649+wahyuals@users.noreply.github.com>
2023-12-11 00:51:29 +0700

    Pertama
    Baru terdapat beberapa activity seperti MainActivity, CameraScanActivity,  DetailItemActivity, FavoriteActivity, ItemListActivity.
    
    Namun baru code layout xml nya sedangkan untuk logic nya belum

---
+++ SoDARIApp/SoDARIApp/app/src/main/java/com/wahyu/sodariapp/CameraScanActivity.kt
@@ -0,0 +1,11 @@
+package com.wahyu.sodariapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_scan)
+    }
+}
---
+++ SoDARIApp/SoDARIApp/app/src/main/java/com/wahyu/sodariapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wahyu.sodariapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
